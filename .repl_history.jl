f30a421154d6a35086f9e504e9571751a4b4a446
j-fu <juergen-fuhrmann@web.de>
2021-11-13 23:13:11 +0000

    Deploying to gh-pages from  @ 9da4580ce28c19d9ad90226f9a97fc76437b8507 🚀

---
+++ .repl_history.jl
@@ -0,0 +1,42 @@
+# time: 2021-11-11 22:08:40 CET
+# mode: pkg
+	serve
+# time: 2021-11-11 22:08:51 CET
+# mode: julia
+	using Franklin
+# time: 2021-11-11 22:08:58 CET
+# mode: julia
+	serve()
+# time: 2021-11-13 23:26:52 CET
+# mode: shell
+	rm QUARRY
+# time: 2021-11-13 23:26:55 CET
+# mode: julia
+	serve()
+# time: 2021-11-13 23:27:01 CET
+# mode: shell
+	ls
+# time: 2021-11-13 23:27:35 CET
+# mode: shell
+	rm __site/QUARRY
+# time: 2021-11-13 23:27:40 CET
+# mode: shell
+	rm -r __site/QUARRY
+# time: 2021-11-13 23:27:42 CET
+# mode: julia
+	serve()
+# time: 2021-11-13 23:27:57 CET
+# mode: julia
+	serve(port=8000)
+# time: 2021-11-13 23:28:03 CET
+# mode: julia
+	serve(port=8001)
+# time: 2021-11-13 23:31:18 CET
+# mode: julia
+	serve()
+# time: 2021-11-13 23:39:19 CET
+# mode: julia
+	serve(port=8001)
+# time: 2021-11-13 23:41:00 CET
+# mode: julia
+	serve(port=8000)
---
+++ assets/genproject.jl
@@ -0,0 +1,266 @@
+#!/bin/sh
+# Run this file via
+# julia --compile=min  genproject.jl projectname 
+#
+
+#
+# This code is reproduced under the MIT License (c) 2021 J. Fuhrmann 
+#
+
+#=
+exec julia --compile=min --startup-file=no "$0" "$@"
+=#
+
+
+
+using ArgParse
+using Pkg
+
+MITLicense()="""
+Copyright <YEAR> <COPYRIGHT HOLDER>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+
+README(name)="""
+$(name)
+======
+
+## Initial remarks
+
+This is the initial version of the project.
+
+### Running code
+
+Invoke the scripts from a running Julia instance in the package root directory.
+Don't forget to activate the project environment by invoking Julia via
+```
+julia --project=.
+```
+
+or by invoking
+```
+using Pkg
+Pkg.activate(".")
+```
+
+### Adaptation
+- Please check the LICENSE file and replace it by another license if you don't agree with it.
+- Please check the `authors` entry in `Project.toml`
+- Remove or replace demo scripts, notebooks and `packages/MySubPackage`
+- Set up a git repo for sharing the code. All subdirectories and top level files in this initial version should be under version control
+- Consider introducing  subdirectories for large simulation results which are not under version control due to their size
+- Consider introducing further subdirectories to struture your project
+- May be DrWatson.jl can be helpful for you. This project structure is compatible to it with two exceptions:
+  - It doesn't rely on `@quickactivate`.  It assumes  that the skills obtained by learning how to work with Julia environments are useful
+  - Notebooks are assumed to be Pluto notebooks which can be  version controlled in straigtforward way, unlike Jupyter notebooks
+
+### Initial file structure
+
+
+```
+MyProject/
+├── Manifest.toml
+├── notebooks
+│   └── presentation.jl
+├── packages
+│   └── MySubPackage
+│       ├── Project.toml
+│       └── src
+│       │    └── MySubPackage.jl
+│       └── test
+│           └── runtests.jl
+├── papers
+│   └── publication.tex
+├── Project.toml
+├── scripts
+│   └── example.jl
+├── src
+│   └── MyProject.jl
+└── test
+    └── runtests.jl
+```
+The essential role of these files is as follows:
+- `Project.toml`: The project file describes the project on a high level, for example the package/project dependencies and compatibility constraints are listed in the project file. See the [documentation](https://pkgdocs.julialang.org/v1/toml-files/#Project-and-Manifest)
+- `Manifest.toml`:  The manifest file is an absolute record of the state of the packages in the environment. It includes exact information about (direct and indirect) dependencies of the project. Given a Project.toml + Manifest.toml pair, it is possible to instantiate the exact same package environment, which is very useful for reproducibility. See the [documentation](https://pkgdocs.julialang.org/v1/toml-files/#Manifest.toml)
+- `LICENSE`: License of the project. By default it is the MIT license
+- `README.md`: This file
+- `src`: Subdirectory for project specific code as part of the $(name) package representing the project.
+- `test`: Unit tests for project code in `src`. Could include something from `scripts`, `notebooks`.
+- `packages`: Subdirectory containing packages developed locally in this project. At some stage these could be turned into separate Julia packages
+- `scripts`, `notebooks`: "Frontend" code for creating project results
+- `papers`: "Wer schreibt, der bleibt" - besides of coding, you probably should publish your results...
+- `docs`: not in this template, but there belong the sources for the docs created with `Documenter.jl`
+"""
+
+
+
+demonotebook(name)="""### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 60941eaa-1aea-11eb-1277-97b991548781
+begin 
+	using Pkg
+	Pkg.activate(joinpath(@__DIR__,".."))
+	using Revise
+	using PlutoUI
+	using $(name)
+	using MySubPackage
+end
+
+# ╔═╡ 882dda23-63b9-4b1e-a04e-69071deff69a
+md"This notebook is only relocateable together with the whole $(name) project."
+
+# ╔═╡ a8e37976-5db2-485f-87aa-0cf7155e8e00
+$(name).greet()
+
+# ╔═╡ 73a94305-1c3c-45e5-969f-2a245baec10d
+MySubPackage.greet()
+
+# ╔═╡ Cell order:
+# ╠═882dda23-63b9-4b1e-a04e-69071deff69a
+# ╠═60941eaa-1aea-11eb-1277-97b991548781
+# ╠═a8e37976-5db2-485f-87aa-0cf7155e8e00
+# ╠═73a94305-1c3c-45e5-969f-2a245baec10d
+"""
+
+demoscript(name)="""            
+module DemoScript
+using $(name)
+using MySubPackage
+    
+function main()
+   $(name).greet()
+   MySubPackage.greet()
+end
+
+end
+"""
+
+
+maketest(name)="""
+using Test
+using $(name)
+using MySubPackage
+
+$(name).greet()
+
+@testset "$(name)" begin
+@test true
+end
+"""
+
+makesubtest()="""
+using Test
+using MySubPackage
+
+MySubPackage.greet()
+
+@testset "MySubPackage" begin
+@test true
+end
+"""
+
+
+
+function main(args)
+    settings = ArgParseSettings()
+    settings.description = "Generate a project template"
+    settings.commands_are_required = false
+    settings.add_version = true
+    add_arg_table(settings,
+                  "name", Dict(:help => "Project name",:required => true),
+                  )
+    parsed_args=parse_args(args, settings)
+    name=parsed_args["name"]
+    
+
+    @info "Generate  $(name)"
+    Pkg.generate(name)
+    
+    cd(name) do
+        @info "Activate  $(name)"
+        Pkg.activate(".")
+        Pkg.add(["Revise","PlutoUI","Pluto","Test"])
+
+        @info "Generate  packages subdirectory"
+        mkdir("packages")
+        cd("packages") do
+            Pkg.generate("MySubPackage")
+            cd("MySubPackage") do
+                Pkg.activate(".")
+                Pkg.add("Test")
+                mkdir("test")
+                cd("test") do
+                    open("runtests.jl", "w") do io
+                        write(io,makesubtest())
+                    end
+                end
+            end
+        end
+        Pkg.activate(".")
+
+        
+        Pkg.develop(path="packages/MySubPackage")
+
+        @info "Generate demo notebook"
+        mkdir("notebooks")
+        cd("notebooks") do
+            open("demo-notebook.jl", "w") do io
+                write(io,demonotebook(name))
+            end
+        end
+
+        @info "Generate demo script"
+        mkdir("scripts")
+        cd("scripts") do
+            open("demo-script.jl", "w") do io
+                write(io,demoscript(name))
+            end
+        end
+        
+        @info "Generate tests"
+        mkdir("test")
+        cd("test") do
+            open("runtests.jl", "w") do io
+                write(io,maketest(name))
+            end
+        end
+
+        @info "Run tests"
+        Pkg.test()
+        Pkg.test("MySubPackage")
+
+        @info "Generate papers, license, readme"
+        mkdir("papers")
+
+        open("LICENSE", "w") do io
+            write(io,MITLicense())
+        end
+        
+        open("README.md", "w") do io
+            write(io,README(name))
+        end
+
+        @info "run demo script"
+        include(joinpath(@__DIR__,name,"scripts","demo-script.jl"))
+        Base.invokelatest(DemoScript.main)
+        println()
+        
+        @info "run demo notebook"
+        include(joinpath(@__DIR__,name,"notebooks","demo-notebook.jl"))
+        println()
+    end
+
+end
+
+main(ARGS)
