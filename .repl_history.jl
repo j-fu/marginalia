c52f1952c4fded0dc8c03c9bd7102e28c47e561e
j-fu <juergen-fuhrmann@web.de>
2022-12-07 09:01:20 +0000

    Deploying to gh-pages from  @ d4539b83e8ba3aef6d36abdfa4b133fffd12132a 🚀

--- .repl_history.jl
+++ .repl_history.jl
@@ -19,3 +19,24 @@
 # time: 2022-11-06 16:28:39 CET
 # mode: julia
 	publish()
+# time: 2022-11-10 00:07:24 CET
+# mode: julia
+	ll
+# time: 2022-11-28 17:11:03 CET
+# mode: pkg
+	up
+# time: 2022-11-28 17:12:42 CET
+# mode: julia
+	serve()
+# time: 2022-12-06 23:43:30 CET
+# mode: pkg
+	up
+# time: 2022-12-06 23:44:04 CET
+# mode: pkg
+	serve()
+# time: 2022-12-06 23:44:09 CET
+# mode: julia
+	serve()
+# time: 2022-12-07 09:59:36 CET
+# mode: julia
+	publish()
