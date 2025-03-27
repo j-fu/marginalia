312dc3c75a8525cbc1b1b9af94b1072fe07f2451
j-fu <juergen-fuhrmann@web.de>
2025-03-27 15:13:06 +0000

    Deploying to gh-pages from  @ 59125573a8a739becedcd19cabd6de3964ace0d5 🚀

--- assets/project-skeleton/notebooks/demo-notebook.jl
+++ assets/project-skeleton/notebooks/demo-notebook.jl
@@ -13,14 +13,23 @@ begin
 	using {PKGNAME}
 end
 
+# ╔═╡ 5b7da564-8c8e-45c9-8c72-534c5a13301d
+md"""
+# Demo notebook for {PKGNAME}
+"""
+
 # ╔═╡ 882dda23-63b9-4b1e-a04e-69071deff69a
-md"This notebook is only relocateable together with the whole {PKGNAME} project."
+md"""
+This notebook is only relocateable together with the whole {PKGNAME} project.
+All packages used by this notebook need to be added to the environment in 
+$(dirname(joinpath(@__DIR__,".."))).
+"""
 
 # ╔═╡ a8e37976-5db2-485f-87aa-0cf7155e8e00
 {PKGNAME}.greet()
 
-
 # ╔═╡ Cell order:
-# ╠═882dda23-63b9-4b1e-a04e-69071deff69a
+# ╟─5b7da564-8c8e-45c9-8c72-534c5a13301d
+# ╟─882dda23-63b9-4b1e-a04e-69071deff69a
 # ╠═60941eaa-1aea-11eb-1277-97b991548781
 # ╠═a8e37976-5db2-485f-87aa-0cf7155e8e00
